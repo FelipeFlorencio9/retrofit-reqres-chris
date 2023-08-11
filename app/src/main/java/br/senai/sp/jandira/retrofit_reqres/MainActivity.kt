5dd6ac0053731657362be272782cac57995dac84
Felipe Florencio <107069825+FelipeFlorencio9@users.noreply.github.com>
2023-08-11 11:22:14 -0300

    Implementado Corrotina no MainActivity

--- app/src/main/java/br/senai/sp/jandira/retrofit_reqres/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/retrofit_reqres/MainActivity.kt
@@ -2,10 +2,39 @@ package br.senai.sp.jandira.retrofit_reqres
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
+
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var apiService: ApiService
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        apiService = RetrofitHelper.getInstance().create(ApiService::class.java)
+
+        //AÇÃO DO BOTÃO GET:
+        findViewById<Button>(R.id.btnGET).setOnClickListener {
+            getUserByID()
+        }
     }
+    private fun getUserByID(){
+
+        lifecycleScope.launch {
+            //CHAMADA PARA O ENDPOINT
+            val result = apiService.getUserByID("2")
+            if(result.isSuccessful){
+                Log.e("GETTING-DATA", "${result.body()?.data}")
+            }else {
+                Log.e("GETTING-DATA", result.message())
+            }
+        }
+
+    }
+
 }
